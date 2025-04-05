b49cb10d3bdb78ff73b115f0fd40cfa5d460174c
Sarina Ebrahimi <156429744+sarinaprt@users.noreply.github.com>
2025-04-05 22:17:17 +0330

    Update DDL.py

--- DDL.py
+++ DDL.py
@@ -1,10 +1,10 @@
 from mysql.connector import connection, Error
 
-def insert_user(USERNAME,password,CREATE_AT=None):
+def insert_user(USERNAME,password):
     config={'user':'root','password':'password','host':'localhost','database':'system_log'}
     conn=connection.MySQLConnection(**config)
     cur=conn.cursor()
-    SQL_QUERY="INSERT INTO USERS(USERNAME,EMAIL,PASSWORD,CREAT_AT) VALUES (%s, %s, %s)"
+    SQL_QUERY="INSERT INTO USERS(USERNAME,PASSWORD) VALUES ( %s, %s)"
 
     cur.execute(SQL_QUERY,(USERNAME, password, CREATE_AT)) 
     conn.commit()
